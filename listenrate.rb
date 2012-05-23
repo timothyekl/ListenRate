daf0cbd7334c8686b4a90d662355cbb29d736a0e
Tim Ekl <lithium3141@gmail.com>
2012-05-23 16:47:59 -0400

    Implement Last.fm's redirection/authentication
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- listenrate.rb
+++ listenrate.rb
@@ -7,9 +7,6 @@ require 'haml'
 require 'lastfm'
 require 'parseconfig'
 
-# Debugging
-require 'pp'
-
 # Local files
 Dir['./models/*.rb'].each {|f| require f}
 
@@ -18,8 +15,8 @@ class ListenRate < Sinatra::Base
   configure do
     # Set up lastfm global handler/interface
     config = ParseConfig.new(File.join(File.dirname(__FILE__), "lastfm-keys.conf"))
+    $api_key = config.params['apikey']
     $lastfm = Lastfm.new(config.params['apikey'], config.params['secret'])
-    $token = $lastfm.auth.get_token
 
     # Configure DataMapper
     DataMapper.setup(:default, "sqlite://#{Dir.pwd}/users.sqlite")
@@ -35,13 +32,29 @@ class ListenRate < Sinatra::Base
     username = params[:username]
     session = Session.first(:username => username)
     if session.nil?
-      # TODO handle the redirection crap
-      "Need to do magic"
+      # Redirect to Last.fm for user authentication
+      callback = "#{request.url}/configure"
+      url = "http://www.last.fm/api/auth/?api_key=#{$api_key}&cb=#{callback}"
+      redirect url
     else
       $lastfm.session = session.session_key
-      pp $lastfm.user.get_info(username)
-      "It worked!"
+      $lastfm.user.get_info(username)
     end
+  end
+
+  get '/info/:username/configure' do
+    # This is the authentication callback URL from Last.fm's API
+    # We need to set up and remember a session key for the given user
+    username = params[:username]
+    token = params[:token] # this token is temporary to this request only
+    session = Session.first_or_create(:username => username)
+
+    session_key = $lastfm.auth.get_session(token)['key']
+
+    session.session_key = session_key
+    result = session.save
+
+    redirect "/info/#{username}"
   end
 
   get '/style.css' do
